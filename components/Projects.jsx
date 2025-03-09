a95fe4a741746702d83f0a6d22b05d4f8398c1da
kawe-education <to379@drexel.edu>
2025-03-08 22:46:31 -0500

    card components changes for Highlighted Project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,40 +1,69 @@
 import React from 'react'
-import { assets } from '@/assets/assets'
+import Link from 'next/link'
 import Image from 'next/image'
+import { assets } from '@/assets/assets'
+import { Button } from "@/components/ui/button"
 const Projects = () => {
     const myProjects = [
         {
             name: "Kawe",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.",
-            tools:["NextJS"]
+            description: "Create personalized study plans for your classes, powered by AI, that adapt to your learning style",
+            tools: ["NextJS", "Figma"],
+            url: "https://www.kawe.app/"
 
         },
         {
-            name: "Meal App",
-            description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil."
+            name: "Automated Study Worksheets",
+            description: "Automatically generate study worksheets for any subject or topic with just a few clicks. ",
+            tools: ["NextJS", "PostgreSQL",],
+            url: "https://github.com/toluodunuga23/study-worksheet-app"
+
+        },
+
+
+
+        {
+            name: "Ultimate Meal App",
+            description: "One-stop shop for meal planning, grocery shopping, and recipe management and meal spots near you.",
+            tools: ["NextJS"],
+            url: "https://github.com/toluodunuga23/meal-app"
 
         },
 
     ]
+
+
     return (
         <>
-            <div className=' max-w-6xl mx-auto h-screen flex-col items-center justify-center gap-8 mt-12'>
+            <div className=' max-w-5xl mx-auto h-screen flex-col items-center justify-center gap-5 mt-12 '>
                 <h1 className="font-Ovo text-3xl text-center">Highlighted Projects </h1>
 
-                <div className='grid grid-cols-2 gap-4 mt-6 place-items-center  '>
+                <div className='grid grid-cols-3  gap-4 place-items-center mt-6 '>
                     {myProjects.map((project, index) => (
-                        <div key={index} className='max-w-sm rounded-lg shadow-lg px-6 pb-6 pl-2 border-2 border-grey-500 '>
+                        <div key={index} className='max-w-sm rounded-lg shadow-lg px-6 pb-6 pl-2 border-2 border-grey-500  h-64'>
                             <div>
                                 <div className="font-bold text-xl mb-2 ml-3 mt-2">{project.name}</div>
                             </div>
                             <p className="text-gray-700 text-base ml-3">
-                              {project.description}
+                                {project.description}
                             </p>
-                            <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#photography</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#travel</span>
-                        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
-                    </div>
+                            <div key={index} className=" mt-4 flex items-end flex-wrap gap-1">
+                                {project.tools.map((tool, index) => (
+                                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-5 mb-2" key={index}>{tool}</span>
+                                ))}
+                            </div>
+                            <Link href={project.url} target="_blank">
+                                <Button className='w-23 mt-8 bg-white text-black border border-gray-600 cursor-pointer hover:bg-slate-200'>{project.name != "Kawe" ? (
+                                    <>
+                                        <Image src={assets.github} alt="" className='rounded-md h-7 w-7' /> View Project
+                                    </>
+                                ) : <>
+                                    <Image src={assets.web_icon_dark} alt="" className='rounded-md h-7 w-7' /> View Website
+                                </>}
+                                </Button>
+
+                            </Link>
+
                         </div>
                     ))}
                 </div>
